61559481044a99fb7b6eddf9fe92b561bb6975e1
whsv26 <whsv26@gmail.com>
2022-01-09 22:43:30 +0300

    Static test for stream filtering.

--- tests/Static/Stream/StreamStaticTest.php
+++ tests/Static/Stream/StreamStaticTest.php
@@ -49,4 +49,22 @@ final class StreamStaticTest
     {
         return $input->mapValues(fn(Foo $val) => $val->a);
     }
+
+    /**
+     * @param Stream<array{array-key, Foo}> $input
+     * @return Stream<array{string, Foo}>
+     */
+    public function testFilterKeys(Stream $input): Stream
+    {
+        return $input->filterKeys(fn($key) => is_string($key));
+    }
+
+    /**
+     * @param Stream<array{string, int}> $input
+     * @return Stream<array{string, 1}>
+     */
+    public function testFilterValues(Stream $input): Stream
+    {
+        return $input->filterValues(fn($val) => 1 === $val);
+    }
 }
