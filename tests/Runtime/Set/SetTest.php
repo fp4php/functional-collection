8c88734a67653db7acec58e274017c4dae655280
whsv26 <whsv26@gmail.com>
2022-01-12 23:12:40 +0300

    Set full coverage.

--- tests/Runtime/Set/SetTest.php
+++ tests/Runtime/Set/SetTest.php
@@ -15,6 +15,19 @@ final class SetTest extends TestCase
             [1, 2, 3],
             HashSet::collect([1, 2, 3, 3])->toList(),
         );
+
+        $this->assertEquals(
+            [1],
+            HashSet::singleton(1)->toList(),
+        );
+
+        $this->assertEquals(
+            [],
+            HashSet::empty()->toList(),
+        );
+
+        $this->assertTrue(HashSet::empty()->isEmpty());
+        $this->assertFalse(HashSet::empty()->isNonEmpty());
     }
 
     public function testCount(): void
